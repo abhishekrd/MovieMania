95f99e13de789af20a259571314993897f05e9e5
Abhishek Dhanke <abhishekrdhanke636@gmail.com>
2022-06-29 19:22:31 +0530

    api bug resolve

--- src/components/useExtract.js
+++ src/components/useExtract.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
 
-export const API_URL =  `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`
+export const API_URL =  `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`
 
 
 const useExtract = (apiParams) => {
